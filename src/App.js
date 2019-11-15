5cd9dd3c0c182fbb2b77ea8dc141a052f7409c90
Zhe Zhang <zhezhang@slac.stanford.edu>
2019-11-14 18:10:50 -0800

    Test the new introduced react hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+// import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Welcome to SPEAR3 ML Platform
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Algorithm Monitor
-      </a>
-    </header>
-  </div>
-)
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to SPEAR3 ML Platform
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Algorithm Monitor
+        </a>
+        <div>
+          <p>You clicked {count} times</p>
+          <button onClick={() => setCount(count + 1)}>
+            Click me
+          </button>
+        </div>
+      </header>
+    </div>
+  )
+}
 
 export default App
