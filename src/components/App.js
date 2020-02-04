fec0b1f9a1bd309004982b567b3f9b24dd53e10a
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-02-04 15:22:55 -0800

    Now the basic boilerplate works!

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,28 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
 import styled from 'styled-components'
 
+import Home from './Home'
 import About from './About'
-import { selectCount } from '../redux/selectors'
-import { increaseCount } from '../redux/actions'
 
 const AppDiv = styled.div`
   text-align: center;
 `
 
-const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`
-
-const AppLink = styled.a`
-  color: #09d3ac;
-`
-
 const App = () => {
-  const count = useSelector(selectCount)
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`
-  })
-
   return (
     <BrowserRouter>
       <AppDiv>
-        <AppHeader>
-          <p>
-            Welcome to SPEAR3 ML Platform
-          </p>
-          <AppLink
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Algorithm Monitor
-          </AppLink>
-          <Link to='/about'>About</Link>
-          <Switch>
-            <Route path='/about'>
-              <About />
-            </Route>
-          </Switch>
-          <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => dispatch(increaseCount())}>
-              Click me
-            </button>
-          </div>
-        </AppHeader>
+        <Link to='/about'>About</Link>
+        <Link to='/'>Home</Link>
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
       </AppDiv>
     </BrowserRouter>
   )
---
+++ src/components/Home.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import styled from 'styled-components'
+
+import { selectCount } from '../redux/selectors'
+import { increaseCount } from '../redux/actions'
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const Link = styled.a`
+  color: #09d3ac;
+`
+
+const Home = () => {
+  const count = useSelector(selectCount)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`
+  })
+
+  return (
+    <Header>
+      <p>
+        Welcome to SPEAR3 ML Platform
+      </p>
+      <Link
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Algorithm Monitor
+      </Link>
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => dispatch(increaseCount())}>
+          Click me
+        </button>
+      </div>
+    </Header>
+  )
+}
+
+export default Home
