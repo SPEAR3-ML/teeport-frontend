24ceea0c5838acb8c8502801608a906d53f31f11
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-02-04 19:38:18 -0800

    Refine global style

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import * as serviceWorker from './utils/serviceWorker'
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
+    padding: 0;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
       "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
       sans-serif;
