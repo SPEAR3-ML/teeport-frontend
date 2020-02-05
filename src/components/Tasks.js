c5cf1c650489a34f24e71b3c76d1ffd0cf46adf3
Zhe Zhang <wenatuhs@gmail.com>
2020-02-05 01:12:50 -0800

    Attempt to add plotly for visualization

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Plot from 'react-plotly.js'
 import styled from 'styled-components'
 
 const CenterDiv = styled.div`
@@ -15,7 +16,29 @@ const AboutDiv = styled(CenterDiv)`
 const Tasks = () => {
   return (
     <AboutDiv>
-      Tasks
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {
+              color: 'red',
+            },
+          },
+          {
+            type: 'bar',
+            x: [1, 2, 3],
+            y: [2, 5, 3],
+          },
+        ]}
+        layout={{
+          width: 320,
+          height: 240,
+          title: 'A Fancy Plot',
+        }}
+      />
     </AboutDiv>
   )
 }
