eaf21d883c8e6febd05c895e5f600b1f451d8f2c
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-02-05 18:55:41 -0800

    Use grid layout in tasks page

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,35 +1,68 @@
 import React from 'react'
 import Plot from 'react-plotly.js'
+import styled from 'styled-components'
+import GridLayout, { WidthProvider } from 'react-grid-layout'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
 
-import { CenterDiv } from './Utils'
+import { DraggableDiv } from './Utils'
+
+const ReactGridLayout = WidthProvider(GridLayout)
+
+const AutoResizePlot = styled(Plot)`
+  width: 100%;
+  height: 100%;
+`
 
 const Tasks = () => {
+  const layout = [
+    { i: '1', x: 0, y: 0, w: 6, h: 12, minW: 3, maxW: 12, minH: 8, maxH: 24 },
+    { i: '2', x: 6, y: 0, w: 6, h: 12, minW: 3, maxW: 12, minH: 8, maxH: 24 },
+    { i: '3', x: 0, y: 12, w: 6, h: 12, minW: 3, maxW: 12, minH: 8, maxH: 24 },
+  ]
+
   return (
-    <CenterDiv>
-      <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {
-              color: 'red',
-            },
-          },
-          {
-            type: 'bar',
-            x: [1, 2, 3],
-            y: [2, 5, 3],
-          },
-        ]}
-        layout={{
-          width: 320,
-          height: 240,
-          title: 'A Fancy Plot',
-        }}
-      />
-    </CenterDiv>
+    <ReactGridLayout
+      className='layout'
+      layout={layout}
+      cols={12}
+      rowHeight={24}
+      draggableHandle='.drag-handler'
+    >
+      <div key='1'>
+        <DraggableDiv>
+          <AutoResizePlot
+            useResizeHandler={true}
+            data={[
+              {
+                x: [1, 2, 3],
+                y: [2, 6, 3],
+                type: 'scatter',
+                mode: 'lines+markers',
+                marker: {
+                  color: 'red',
+                },
+              },
+              {
+                type: 'bar',
+                x: [1, 2, 3],
+                y: [2, 5, 3],
+              },
+            ]}
+            layout={{
+              autosize: true,
+              title: 'A Fancy Plot',
+            }}
+          />
+        </DraggableDiv>
+      </div>
+      <div key='2'>
+        <DraggableDiv>2</DraggableDiv>
+      </div>
+      <div key='3'>
+        <DraggableDiv>3</DraggableDiv>
+      </div>
+    </ReactGridLayout>
   )
 }
 
--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import styled from 'styled-components'
 
 export const CenterDiv = styled.div`
@@ -6,3 +7,34 @@ export const CenterDiv = styled.div`
   top: 50%;
   transform: translateY(-50%);
 `
+
+const FillDiv = styled.div`
+  position: relative;
+  height: 100%;
+`
+
+const HandlerDiv = styled.div`
+  height: 16px;
+  border-radius: 4px 4px 0px 0px;
+  background-color: #A79E70;
+`
+
+const ContentDiv = styled.div`
+  position: absolute;
+  top: 16px;
+  bottom: 0px;
+  width: 100%;
+  background-color: #4D4F53;
+`
+
+export const DraggableDiv = props => {
+  return (
+    <FillDiv>
+      <HandlerDiv className='drag-handler'>
+      </HandlerDiv>
+      <ContentDiv>
+        {props.children}
+      </ContentDiv>
+    </FillDiv>
+  )
+}
