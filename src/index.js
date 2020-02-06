550e128682612aebfe655c134b5b2419ad16de0e
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-02-05 19:38:50 -0800

    Remove useless comments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createGlobalStyle } from 'styled-components'
 
-// import './index.css'
 import App from './components/App'
 import store from './redux/store'
 
