f2114ad4f147a15aa0da84e0c4797ad9f1d0aef8
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-02-15 12:14:03 -0800

    Add SLAC palette for better theming

---
+++ src/plugins/slacPalette.js
@@ -0,0 +1,20 @@
+const palette = {
+  red: {
+    normal: '#8C1515',
+    light: '#BD8282',
+    dark: '#751212',
+  },
+  yellow: {
+    normal: '#D2C295',
+    dark: '#A79E70',
+  },
+  grey: {
+    normal: '#4D4F53',
+  },
+}
+
+export const red = palette.red
+export const yellow = palette.yellow
+export const grey = palette.grey
+
+export default palette
