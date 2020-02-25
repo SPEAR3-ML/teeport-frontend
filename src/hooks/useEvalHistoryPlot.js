c0fd755ea478cb04b89d74d10819e6e617f08a8c
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-02-24 20:55:46 -0800

    Add min curves in the eval history plot

--- src/hooks/useEvalHistoryPlot.js
+++ src/hooks/useEvalHistoryPlot.js
@@ -1,22 +1,36 @@
 import React, { useState, useEffect, useCallback } from 'react'
+// import Plotly from 'plotly.js/lib/core'
 import _ from 'lodash'
 
 import { AutoResizePlot } from '../components/Utils'
 
-const getXY = history => {
+// const palette = Plotly.d3.scale.category10().range()
+
+const getXObjsBests = history => {
   let x = []
   let y = []
+  const objs = []
+  const bests = []
 
-  if (!history) {
-    return [x, y]
+  if (history) {
+    history.forEach(([, Y]) => {
+      y = _.concat(y, Y)
+    })
+    x = _.range(1, _.size(y) + 1)
+    for (let i = 0; i < y[0].length; i++) {
+      const obj = y.map(p => p[i])
+      objs.push(obj)
+      const best = []
+      let min = obj[0]
+      for (let j = 0; j < obj.length; j++) {
+        min = Math.min(min, obj[j])
+        best.push(min)
+      }
+      bests.push(best)
+    }
   }
 
-  history.forEach(([, Y]) => {
-    y = _.concat(y, Y)
-  })
-  x = _.range(1, _.size(y) + 1)
-
-  return [x, y]
+  return [x, objs, bests]
 }
 
 const useEvalHistoryPlot = task => {
@@ -52,13 +66,13 @@ const useEvalHistoryPlot = task => {
   }, [setRevision])
 
   useEffect(() => {
-    const [x, Y] = getXY(task.history)
+    const [x, objs, bests] = getXObjsBests(task.history)
     if (x.length) {
       const data = []
-      for (let i = 0; i < Y[0].length; i++) {
+      for (let i = 0; i < objs.length; i++) {
         data.push({
           x,
-          y: Y.map(p => p[i]),
+          y: objs[i],
           type: 'scatter',
           mode: 'lines+markers',
           name: `obj${i + 1}`,
@@ -66,9 +80,27 @@ const useEvalHistoryPlot = task => {
             opacity: 0.4,
           },
         })
+        data.push({
+          x,
+          y: bests[i],
+          type: 'scatter',
+          mode: 'lines',
+          name: `obj${i + 1} min`,
+          line: {
+            dash: 'dashdot',
+            // width: 3,
+          },
+        })
       }
       setFigure(f => {
-        f.data = data
+        if (!f.data.length) {
+          f.data = data
+        } else {
+          f.data.forEach((trace, i) => {
+            trace.x = data[i].x
+            trace.y = data[i].y
+          })
+        }
         return f
       })
       setRevision(r => r + 1)
