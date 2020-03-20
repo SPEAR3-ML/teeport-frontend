5dff39949265a79269e46839edec744673fb515d
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-03-20 12:01:21 -0700

    Support serve on local and on docker Also adapt to the /teeport basename

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const path = require('path')
 
+let buildDir = path.join(__dirname, 'build')
+if (process.env.MODE === 'local') {
+  buildDir = path.join(__dirname, '..', 'build')
+}
+
 const app = express()
-app.use(express.static(path.join(__dirname, 'build')))
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+app.use('/teeport', express.static(buildDir))
+app.get('/teeport/*', (req, res) => {
+  res.sendFile(path.join(buildDir, 'index.html'))
 })
 app.listen(3000)
