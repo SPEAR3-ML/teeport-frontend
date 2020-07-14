f712939e0cd86e4bd307061681f4fcd6da509a09
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-07-14 13:54:21 -0700

    Add more colors into the SLAC palette

--- src/plugins/slacPalette.js
+++ src/plugins/slacPalette.js
@@ -11,10 +11,30 @@ const palette = {
   grey: {
     normal: '#4D4F53',
   },
+  orange: {
+    normal: '#E17000',
+    dark: 'C75B12',
+  },
+  purple: {
+    normal: '#6E2585',
+    dark: '#5B1F69',
+  },
+  blue: {
+    normal: '#0099CC',
+    dark: '#006983',
+  },
+  green: {
+    normal: '#69BE28',
+    dark: '#5B8F22',
+  },
 }
 
 export const red = palette.red
 export const yellow = palette.yellow
 export const grey = palette.grey
+export const orange = palette.orange
+export const purple = palette.purple
+export const blue = palette.blue
+export const green = palette.green
 
 export default palette
