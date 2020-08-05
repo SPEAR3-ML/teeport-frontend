bf6ffa6afc7e74cf4fb065e9afa75ca4e54e2845
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-08-04 21:41:41 -0700

    Add a margin setting comment for future use

--- src/components/ResponsiveGrid.js
+++ src/components/ResponsiveGrid.js
@@ -30,6 +30,7 @@ const ResponsiveGrid = ({ children, rowHeight, breakpoints }) => {
       breakpoints={breakpoints}
       cols={{ lg: 12, md: 12, sm: 12, xs: 12, xxs: 12 }}
       rowHeight={rowHeight}
+      // margin={[8, 8]}
       isDraggable={false}
       isResizable={false}
       measureBeforeMount={true}
