e9d90085b9c65d09d7d5ecc8470e031a997a3857
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-08-05 20:12:52 -0700

    Replace the react-select with custom selector for more consistent style

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
 // import Modal from 'react-modal'
 import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
-import Select from 'react-select'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
+// import Select from 'react-select'
 import AceEditor from 'react-ace'
 import 'ace-builds/webpack-resolver'
 import 'ace-builds/src-noconflict/mode-json'
@@ -43,76 +49,118 @@ const NewTask = ({ show, setShow, clients, sendMessage }) => {
         <Modal.Title>Create New Task</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <div style={{ marginBottom: 16 }}>
-          <h6>Choose an optimizer</h6>
-          <Select
-            styles={{ menu: styles => ({ ...styles, zIndex: 999 }) }}
-            value={selectedOptimizer}
-            onChange={selected => {
-              setSelectedOptimizer(selected)
-              const _configs = JSON.parse(configs)
-              if (selected !== null) {
-                const optimizer = clients.filter(client => {
-                  return client.id === selected.value
-                })[0]
-                _configs.optimizer = optimizer.configs
-              } else {
-                delete _configs.optimizer
-              }
-              setConfigs(JSON.stringify(_configs, null, '\t'))
-            }}
-            options={optimizers}
-            isClearable={true}
-          />
-        </div>
-        <div style={{ marginBottom: 16 }}>
-          <h6>Choose an evaluator</h6>
-          <Select
-            styles={{ menu: styles => ({ ...styles, zIndex: 999 }) }}
-            value={selectedEvaluator}
-            onChange={selected => {
-              setSelectedEvaluator(selected)
-              const _configs = JSON.parse(configs)
-              if (selected !== null) {
-                const evaluator = clients.filter(client => {
-                  return client.id === selected.value
-                })[0]
-                _configs.evaluator = evaluator.configs
-              } else {
-                delete _configs.evaluator
-              }
-              setConfigs(JSON.stringify(_configs, null, '\t'))
-            }}
-            options={evaluators}
-            isClearable={true}
-          />
-        </div>
-        <div>
-          <h6>Task configs</h6>
-          <AceEditor
-            mode='json'
-            theme='github'
-            value={configs}
-            onLoad={editor => {
-              editor.renderer.setScrollMargin(8, 8, 8, 8)
-            }}
-            onChange={setConfigs}
-            onValidate={a => setValid(a.every(({ type }) => type !== 'error'))}
-            name='TASK_CONFIGS'
-            // autoScrollEditorIntoView={true}
-            maxLines={12}
-            minLines={12}
-            // height='128px'
-            width='100%'
-            editorProps={{ $blockScrolling: true }}
-          />
-        </div>
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridOptimizer">
+              <Form.Label>Optimizer</Form.Label>
+              <DropdownButton as={ButtonGroup} variant="light"
+                title={selectedOptimizer ? selectedOptimizer.label : 'Select'}
+                style={{ width: '100%' }}
+              >
+                <FormControl
+                  autoFocus
+                  className="mx-3 w-auto"
+                  placeholder="Type to filter..."
+                  onChange={() => {}}
+                />
+                <Dropdown.Divider />
+                {optimizers.map(optimizer => (
+                  <Dropdown.Item key={optimizer.value} eventKey={optimizer.value}
+                    active={selectedOptimizer && selectedOptimizer.value === optimizer.value}
+                    onClick={() => {
+                      const active = selectedOptimizer && selectedOptimizer.value === optimizer.value
+                      const _configs = JSON.parse(configs)
+                      if (active) {
+                        setSelectedOptimizer(null)
+                        delete _configs.optimizer
+                      } else {
+                        setSelectedOptimizer(optimizer)
+                        const _optimizer = clients.filter(client => {
+                          return client.id === optimizer.value
+                        })[0]
+                        _configs.optimizer = _optimizer.configs
+                      }
+                      setConfigs(JSON.stringify(_configs, null, '\t'))
+                    }}
+                  >
+                    <div className="mb-n1">
+                      {optimizer.label}
+                    </div>
+                    <small className="text-info">
+                      {optimizer.value}
+                    </small>
+                  </Dropdown.Item>
+                ))}
+              </DropdownButton>
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridEvaluator">
+              <Form.Label>Evaluator</Form.Label>
+              <DropdownButton as={ButtonGroup} variant="light"
+                title={selectedEvaluator ? selectedEvaluator.label : 'Select'}
+                style={{ width: '100%' }}
+              >
+                <FormControl
+                  autoFocus
+                  className="mx-3 w-auto"
+                  placeholder="Type to filter..."
+                  onChange={() => {}}
+                />
+                <Dropdown.Divider />
+                {evaluators.map(evaluator => (
+                  <Dropdown.Item key={evaluator.value} eventKey={evaluator.value}
+                    active={selectedEvaluator && selectedEvaluator.value === evaluator.value}
+                    onClick={() => {
+                      const active = selectedEvaluator && selectedEvaluator.value === evaluator.value
+                      const _configs = JSON.parse(configs)
+                      if (active) {
+                        setSelectedEvaluator(null)
+                        delete _configs.evaluator
+                      } else {
+                        setSelectedEvaluator(evaluator)
+                        const _evaluator = clients.filter(client => {
+                          return client.id === evaluator.value
+                        })[0]
+                        _configs.evaluator = _evaluator.configs
+                      }
+                      setConfigs(JSON.stringify(_configs, null, '\t'))
+                    }}
+                  >
+                    <div className="mb-n1">
+                      {evaluator.label}
+                    </div>
+                    <small className="text-info">
+                      {evaluator.value}
+                    </small>
+                  </Dropdown.Item>
+                ))}
+              </DropdownButton>
+            </Form.Group>
+          </Form.Row>
+          <Form.Group controlId="formGridConfigs" className="mb-0">
+            <Form.Label>Task Configs</Form.Label>
+            <AceEditor
+              mode='json'
+              theme='github'
+              value={configs}
+              onLoad={editor => {
+                editor.renderer.setScrollMargin(8, 8, 8, 8)
+              }}
+              onChange={setConfigs}
+              onValidate={a => setValid(a.every(({ type }) => type !== 'error'))}
+              name='TASK_CONFIGS'
+              // autoScrollEditorIntoView={true}
+              maxLines={12}
+              minLines={12}
+              // height='128px'
+              width='100%'
+              editorProps={{ $blockScrolling: true }}
+            />
+          </Form.Group>
+        </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant='secondary' onClick={() => setShow(false)}>
-          Close
-        </Button>
-        <Button variant='primary'
+        <Button variant='primary' size='lg'
+          block
           disabled={!selectedOptimizer || !selectedEvaluator || !valid}
           onClick={() => {
             const msg = {
@@ -122,8 +170,16 @@ const NewTask = ({ show, setShow, clients, sendMessage }) => {
               configs: JSON.parse(configs),
             }
             sendMessage(JSON.stringify(msg))
-          }}
-        >
+
+            setShow(false)
+            setSelectedOptimizer(null)
+            setSelectedEvaluator(null)
+            setConfigs(JSON.stringify({
+              task: {
+                name: null,
+              },
+            }, null, '\t'))
+          }}>
           Create Task
         </Button>
       </Modal.Footer>
