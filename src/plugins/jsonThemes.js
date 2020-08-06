5c0cc304a82dcd1b0954d79686a9e08340054442
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-08-06 15:48:47 -0700

    Add the pretty json themes plugin

---
+++ src/plugins/jsonThemes.js
@@ -0,0 +1,14 @@
+const themes = {
+  github: {
+    main: 'line-height:1.3;color:#000000;background:#ffffff;overflow:auto;',
+    error: 'line-height:1.3;color:#66d9ef;background:#272822;overflow:auto;',
+    key: 'color:#000000;',
+    string: 'color:#d14;',
+    value: 'color:#099;',
+    boolean: 'color:#000000;font-weight:bold;',
+  },
+}
+
+export const github = themes.github
+
+export default themes
