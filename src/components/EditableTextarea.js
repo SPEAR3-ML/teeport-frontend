21de57ef5be9ad758da863c2d8e1c5d87355a3ef
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-08-06 01:52:12 -0700

    Create an editable textarea that mimics the input group behavior

---
+++ src/components/EditableTextarea.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+
+const EditableTextarea = ({ current, placeholder, onConfirm }) => {
+  const [text, setText] = useState(current || '')
+  const [editing, setEditing] = useState(false)
+
+  if (editing) {
+    return (
+      <div className='d-flex flex-column flex-grow-1'>
+        <InputGroup className='flex-grow-1'>
+          <FormControl as='textarea' aria-label='Description'
+            style={{
+              resize: 'none',
+              borderRadius: '4px 4px 0 0',
+              marginBottom: -1,
+            }}
+            value={text}
+            placeholder={placeholder}
+            onChange={e => setText(e.target.value)}
+          />
+        </InputGroup>
+        <ButtonGroup aria-label='Desc control'>
+          <Button
+            onClick={() => {
+              setText(current || '')
+              setEditing(false)
+            }}
+            variant='outline-secondary'
+            style={{ borderRadius: '0 0 0 4px' }}
+          >
+            Cancel
+          </Button>
+          <Button disabled={text === (current || '')}
+            onClick={() => {
+              onConfirm(text)
+              setEditing(false)
+            }}
+            variant='outline-secondary'
+            style={{ borderRadius: '0 0 4px 0' }}
+          >
+            Confirm
+          </Button>
+        </ButtonGroup>
+      </div>
+    )
+  } else {
+    return (
+      <div
+        className='h-100 w-100 text-secondary bg-light rounded px-3 py-2 overflow-auto'
+        onClick={() => setEditing(true)}
+        style={{ whiteSpace: 'pre-wrap' }}
+      >
+        <cite>{text}</cite>
+      </div>
+    )
+  }
+}
+
+export default EditableTextarea
