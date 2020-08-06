f1fe5264d960a6f5029fd57700f8d3d4e3535864
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-08-05 21:10:34 -0700

    Extract the Editable card title component

---
+++ src/components/EditableTitle.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+
+const EditableTitle = ({ current, placeholder, onConfirm }) => {
+  const [title, setTitle] = useState(current)
+  const [editing, setEditing] = useState(false)
+
+  return (
+    <Card.Title onClick={editing ? null : () => setEditing(true)}
+      style={{ lineHeight: '38px' }}
+    >
+      {!editing
+        ? title
+        : <InputGroup>
+          <FormControl
+            placeholder={placeholder}
+            aria-label={placeholder}
+            aria-describedby='basic-addon2'
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <InputGroup.Append>
+            <Button variant='outline-secondary' onClick={() => {
+              onConfirm(title)
+              setEditing(false)
+            }} disabled={current === title}>
+              Confirm
+            </Button>
+            <Button variant='outline-secondary'
+              onClick={() => {
+                setTitle(current)
+                setEditing(false)
+              }}
+            >
+              Cancel
+            </Button>
+          </InputGroup.Append>
+        </InputGroup>
+      }
+    </Card.Title>
+  )
+}
+
+export default EditableTitle
