559f02a97edd5efe0d2bf3e0f247775ea8ca94f7
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-09-09 17:09:40 -0700

    Add basename and port configs for the server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const path = require('path')
 
+const stripTrailingSlash = str => {
+  return str.endsWith('/') ? str.slice(0, -1) : str
+}
+
+// setup serve directory
 let buildDir = path.join(__dirname, 'build')
 if (process.env.MODE === 'local') {
   buildDir = path.join(__dirname, '..', 'build')
 }
 
+// setup basename
+const basename = process.env.BASENAME || '/'
+
+// setup port
+const port = parseInt(process.env.PORT) || 3000
+
 const app = express()
-app.use('/teeport', express.static(buildDir))
-app.get('/teeport/*', (req, res) => {
+app.use(basename, express.static(buildDir))
+app.get(stripTrailingSlash(basename) + '/*', (req, res) => {
   res.sendFile(path.join(buildDir, 'index.html'))
 })
-app.listen(3000)
+app.listen(port)
