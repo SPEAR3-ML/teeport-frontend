1bf58a3d4fd2acef144c87a997fcc4ea0836d5ae
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-09-09 17:23:59 -0700

    Enable gzip for faster serving

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const compression = require('compression')
 const path = require('path')
 
 const stripTrailingSlash = str => {
@@ -18,8 +19,11 @@ const basename = process.env.BASENAME || '/'
 const port = parseInt(process.env.PORT) || 3000
 
 const app = express()
+app.use(compression()) // enable gzip
+
 app.use(basename, express.static(buildDir))
 app.get(stripTrailingSlash(basename) + '/*', (req, res) => {
   res.sendFile(path.join(buildDir, 'index.html'))
 })
+
 app.listen(port)
